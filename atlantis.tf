8ab8b34df5fd4656058ccaf69c970780a57ea68a
rhidayat1980 <r.hidayat1980@gmail.com>
2021-01-24 03:58:45 +0700

    added back atlantis

--- atlantis.tf
+++ atlantis.tf
@@ -1,10 +1,10 @@
-# resource "helm_release" "atlantis" {
-#   name       = "atlantis"
-#   repository = "https://runatlantis.github.io/helm-charts"
-#   chart      = "atlantis"
-#   namespace  = "ndigital"
+resource "helm_release" "atlantis" {
+  name       = "atlantis"
+  repository = "https://runatlantis.github.io/helm-charts"
+  chart      = "atlantis"
+  namespace  = "ndigital"
 
-#   values = [
-#     file("atlantis.yaml")
-#   ]
-# }
+  values = [
+    file("atlantis.yaml")
+  ]
+}
